7e6a727a1ff24127a72ed64c6b3b52596ae4ac7a
MARCELLE LOHANE GONCALVES GANIMO <202402726056@alunos.ibmec.edu.br>
2024-10-31 10:32:56 -0300

    nova versao

--- src/pages/Fotos.jsx
+++ src/pages/Fotos.jsx
@@ -1,12 +1,23 @@
 import FotoCard from "../components/FotoCard/FotoCard";
 import ListContainer from "../components/ListContainer/ListContainer";
-import dados from "../data/computadores.json";
+import brutos from "../data/computadores.json";
 import Protegida from "./Protegida";
 
 const Fotos = () => {
  
+  const [dados, setDados] = useState(brutos)
+
+  const filtra = (entrada) => {
+    setDados(dados.filter(
+      (ele) => ele.nome.includes(entrada)
+    ))
+  }
+
   return (
       <Protegida>
+        <input type="text"
+         placeholder="Pesquise Aqui"
+         onChange={(e) => filtra(e.target.value)}/>
         <ListContainer>
           {dados.map(
             (el, index) => (
